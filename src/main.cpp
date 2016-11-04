f8586c90d07739a187b03e57c60336b7bac245c4
FrancyGallu <galluzzi.francesca@gmail.com>
2016-11-04 11:03:24 +0000

    Random noise

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <cstdlib>
 #include "Image.h"
+#include <random>
 
 int main()
 {
+    constexpr size_t width = 1024;
+    constexpr size_t height = 1024;
+
     std::cout<<"testing image\n";
-    Image test(200,200);
-    test.clearScreen(255,0,0);
-    test.save("red.png");
+    Image test(width,height);
+
+    std::random_device rd;
+    std::default_random_engine re(rd());
+    std::uniform_int_distribution<> colour(0,255);
+    std::uniform_int_distribution<> posx(0,width-1);
+    std::uniform_int_distribution<> posy(0,height-1);
+
+    test.clearScreen(255,255,255);
+
+    for(int i=0; i<123243346; ++i)
+    {
+        test.setPixel(posx(re), posy(re), colour(re), colour(re), colour(re));
+    }
+
+    test.save("random.png");
+
     return EXIT_SUCCESS;
 }
